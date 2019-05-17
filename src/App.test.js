3461c96bc4ac9c46d4bbb3c0630050c87a194e0e
nadvolod <nadvolod@gmail.com>
2019-05-17 19:15:18 -0400

    1st react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {expect } from 'chai'; //chai is an assertion library used with mocha
+import sinon from 'sinon';  //sinon is a mocking framework for mochaÍ
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
